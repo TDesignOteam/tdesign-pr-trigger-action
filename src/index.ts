ea2c71da34646fc81737db93a679d6aca0ce096d
黎伟杰 <674416404@qq.com>
2025-01-16 08:43:54 +0800

    feat: add comment trigger whitelist

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
 import process from 'node:process'
 import { getInput, info } from '@actions/core'
-
 import { context } from '@actions/github'
 import { setGitConfig } from './utils'
 import useTrigger from './utils/trigger'
@@ -9,13 +10,27 @@ export async function run(): Promise<void> {
   const repo = getInput('repo') || context.repo.repo
   const owner = getInput('owner') || context.repo.owner
   const pr_number = getInput('pr_number') || context.issue.number
-  const token = process.env.GITHUB_TOKEN || getInput('token')
+  const token = getInput('token') || process.env.GITHUB_TOKEN || ''
   const trigger = getInput('trigger') || context.payload.comment?.body || ''
 
   if (context.eventName === 'issue_comment' && context.payload.pull_request) {
     info('pr comment trigger')
+    const whitelist = readFileSync(resolve(__dirname, '../.comment-trigger-whitelist'), 'utf-8')
     // TODO 需要白名单的人才能触发
+    let isWhitelist = false
+
+    whitelist.split('\n').forEach((item) => {
+      if (item.trim() === context.payload.comment?.user.login) {
+        info('comment whitelist trigger')
+        isWhitelist = true
+      }
+    })
+    if (!isWhitelist) {
+      info(`${context.payload.comment?.user.login}不在白名单内，不触发`)
+      return
+    }
   }
+
   await setGitConfig()
 
   useTrigger({
@@ -25,33 +40,5 @@ export async function run(): Promise<void> {
     token,
     trigger,
   })
-  // info('comment', comment)
-  // const repo_url = `https://${token}@github.com/liweijie0812/tdesign-vue-next.git`
-  // await exec(`ls -al`)
-  // await exec(`git clone ${repo_url} ../tdesign-vue-next`)
-  // await exec(`ls -al ../`)
-  // await exec(`ls -al`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`git config --global user.email "github-actions[bot]@users.noreply.github.com""`)
-  // await exec(`git config --global user.name "github-actions[bot]"`)
-  // await exec(`git submodule update --init --remote`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`git remote -v`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`npm install`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`npm run test:update`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`git status`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`git checkout -b chore/update-common/pr${pr_number}`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`git commit -am "chore: update common"`, [], { cwd: `../tdesign-vue-next` })
-  // await exec(`git push origin chore/update-common/pr${pr_number}`, [], { cwd: `../tdesign-vue-next` })
-
-  // await octokit.rest.pulls.create({
-  //   owner: 'liweijie0812',
-  //   repo: 'tdesign-vue-next',
-  //   title: 'chore: update common',
-  //   head: `chore/update-common/pr${pr_number}`,
-  //   base: 'develop',
-  //   body: pr_data.body || '',
-  // })
-  // await exec(`ls -al`)
-//   await exec(`git submodule update --init --remote`)
-//   await exec(`git status`)
 }
 run().catch(console.error)
