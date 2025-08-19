7a507a216fd8edd750a4178335aadb7f6ef7e765
lwj <674416404@qq.com>
2025-08-19 22:33:53 +0800

    chore: build esm

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,5 @@ import packageJson from './package.json' with { type: 'json' }
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs'],
   noExternal: [...Object.keys(packageJson.dependencies)],
 })
